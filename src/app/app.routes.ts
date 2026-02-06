eb854a8bdaedf65490ba904eb2891ecc8be39dbf
Justin Byrne <theskaterjesus@gmail.com>
2026-02-06 21:42:00 +0000

    added custom route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BlogPost } from './blog-post/blog-post';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: ':slug',
+        component: BlogPost,
+    }
+];
