254d9001f92eda117adfd8e0e0f9b9259a1dbd88
Justin Byrne <theskaterjesus@gmail.com>
2026-02-07 14:29:18 +0000

    added routes to the extra pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
-import { BlogPost } from './blog-post/blog-post';
+import { BlogPost } from './pages/blog-post/blog-post';
+import { Home } from './pages/home/home';
+import { Posts } from './pages/posts/posts';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'posts',
+        component: Posts,
+    },
     {
         path: ':slug',
         component: BlogPost,
