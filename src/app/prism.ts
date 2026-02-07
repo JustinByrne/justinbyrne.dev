39c2b3fa62e865ca974301e6f4e43d78cf1dd038
Justin Byrne <theskaterjesus@gmail.com>
2026-02-07 13:00:55 +0000

    added a prism typescript file for the imports

---
+++ src/app/prism.ts
@@ -0,0 +1,19 @@
+import * as Prism from 'prismjs';
+
+// Required base grammars
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-markup-templating';
+
+// Used languages
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-css';
+
+// extras
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+
+export default Prism;
